5d776a249581cef31b63bf26da74be856443f844
Ayman-ing <aymanfeki1@gmail.com>
2024-02-11 14:07:40 +0100

    Configuring Spring Boot to use Java 8 Date/Time converters and UTC Timezone

--- src/main/java/com/example/polls/PollsApplication.java
+++ src/main/java/com/example/polls/PollsApplication.java
@@ -1,12 +1,31 @@
 package com.example.polls;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+//converting Java 8 Date/Time fields in the domain models to SQL types
+// when we persist them in the database
+@EntityScan(basePackageClasses = {
+		PollsApplication.class,
+		Jsr310JpaConverters.class
+})
 public class PollsApplication {
+	//set the default timezone for our application to UTC.
 
+	@PostConstruct
+	void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 	public static void main(String[] args) {
+
 		SpringApplication.run(PollsApplication.class, args);
 	}
 
